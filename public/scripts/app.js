2b2cba15f919063dfa67117e80ab46285448e626
eamoses <e.a.moses@gmail.com>
2016-04-06 20:46:57 -0700

    styled buttons, made modals and grey background

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,15 @@
 $(function(){
       console.log('app.js loaded!');
-      $('.carousel').carousel();
+      //Bootstrap carousel functionality
       $('.carousel').carousel({
-      interval: 2000
+      interval: 5000
       });
+      //***OBJECTIVE BUTTON FUNCTIONALITY***//
+      $(".addCat").on("click", function(){
+        $("div.col-md-6.col-md-offset-3.popup.hidden").removeClass("hidden");//opens popup to explain objective
+      });
+      $(".close").on("click", function(){
+        $("div.col-md-6.col-md-offset-3.popup").addClass("hidden"); //makes popup go hidden again so you can play the game
+      });
+
   });
