26105fc3b3a8f22b88337e509a7479fc6191c6c8
eamoses <e.a.moses@gmail.com>
2016-04-08 11:57:15 -0700

    simple format, just cats listed on page with name and photograph

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -1,7 +1,10 @@
 /************
  * DATABASE *
  ************/
+
 var db = require('../models');
+
+
 
 // GET /api/albums
 function index(req, res) {
@@ -29,24 +32,25 @@ function show(req, res) {
 
 function destroy(req, res) {
   db.Cat.findOneAndRemove({ _id: req.params.catId }, function(err, foundCat){
-      res.json(foundCat);
-    });}
+    res.json(foundCat);
+  });
+}
 
-    function update(req, res) {
-      console.log('updating with data', req.body);
-      db.Cat.findById(req.params.catId, function(err, foundCat) {
-        if(err) { console.log('catsController.update error', err); }
-        foundCat.petName = req.body.petName;
-        foundCat.pictureUrl = req.body.pictureUrl;
-        foundCat.locationLastSeen = req.body.locationLastSeen;
-        foundCat.dateLastSeen = req.body.dateLastSeen;
-        foundCat.save(function(err, savedCat) {
-          if(err) { console.log('saving altered cat failed'); }
-          res.json(savedCat);
-        });
-      });
+function update(req, res) {
+  console.log('updating with data', req.body);
+  db.Cat.findById(req.params.catId, function(err, foundCat) {
+    if(err) { console.log('catsController.update error', err); }
+    foundCat.PetName = req.body.petName;
+    foundCat.pictureUrl = req.body.pictureUrl;
+    foundCat.locationLastSeen = req.body.locationLastSeen;
+    foundCat.dateLastSeen = req.body.dateLastSeen;
+    foundCat.save(function(err, savedCat) {
+      if(err) { console.log('saving altered cat failed'); }
+      res.json(savedCat);
+    });
+  });
 
-    }
+}
 
 
 // export public methods here
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,101 +1,93 @@
-$(function(){
-      console.log('app.js loaded!');
-      //Bootstrap carousel functionality
-      $('.carousel').carousel({
-      interval: 5000
-      });
-      //open and close add cat popup
-      $(".addCat").on("click", function(){
-        $("div.col-md-6.col-md-offset-3.popupPost.hidden").removeClass("hidden");//opens popup
-      });
-      $(".addCatToDb").on("click", function(){
-        $("div.col-md-6.col-md-offset-3.popupPost").addClass("hidden"); //makes popup go hidden
-      });
-      $(".updateCat").on("click", function(){
-        $("div.col-md-6.col-md-offset-3.popupUpdate.hidden").removeClass("hidden");//opens popup
-      });
-      $(".updateCatToDb").on("click", function(){
-        $("div.col-md-6.col-md-offset-3.popupUpdate").addClass("hidden"); //makes popup go hidden again
-      });
-      //info button functionality
-      $(".info").on("click", function(){
-        $(".pop.hidden").removeClass("hidden");//opens popup
-      });
-      $(".close").on("click", function(){
-        $(".pop").addClass("hidden"); //makes popup go hidden
-      });
-      //GETTING ONE CAT ON THE PAGE
-      $.get('/api/cats').success(function (cats) {
-        cats.forEach(function(cat) {
-        renderCat(cat);
-        });
-      });
-      $('#cat-form-add form').on('submit', function(e) {
-      e.preventDefault();
-      var formData = $(this).serialize();
-      console.log('formData', formData);
-      $.post('/api/cats', formData, function(cat) {
-        console.log('cat after POST', cat);
-        renderCat(cat);  //render the server's response
-      });
-      $(this).trigger("reset");
-      });
-      //DELETE CAT
-      $('#cats').on('click', '.deleteCat', handleDeleteCatClick);
+console.log("Sanity Check: JS is working!");
+var template;
+var $catsList;
+var allCats = [];
+
+$(document).ready(function(){
+
+  $catsList = $('#catTarget');
+
+  // compile handlebars template
+  var source = $('#cats-template').html();
+  template = Handlebars.compile(source);
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/cats',
+    success: handleSuccess,
+    error: handleError
+  });
+
+  $('#newCatForm').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/cats',
+      data: $(this).serialize(),
+      success: newCatSuccess,
+      error: newCatError
+    });
+  });
+
+  $catsList.on('click', '.deleteBtn', function() {
+    console.log('clicked delete button to', '/api/cats/'+$(this).attr('data-id'));
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/cats/'+$(this).attr('data-id'),
+      success: deleteCatSuccess,
+      error: deleteCatError
+    });
+  });
+
 });
 
-  // this function takes a single cat and renders it to the page
-  function renderCat(cat) {
-    var carouselArray = [];
-    console.log('rendering cat', cat);
-    var catHtml = $('#cat-template').html();
-    var catsTemplate = Handlebars.compile(catHtml);
-    var html = catsTemplate(cat);
-    $('#cats').prepend(html);
-    carouselArray.push(html);
+// helper function to render all posts to view
+// note: we empty and re-render the collection each time our post data changes
+function render () {
+  // empty existing posts from view
+  $catsList.empty();
+
+  // pass `allBooks` into the template function
+  var catsHtml = template({ cats: allCats });
+
+  // append html to the view
+  $catsList.append(catsHtml);
+}
+
+function handleSuccess(json) {
+  allCats = json;
+  render();
+}
+
+function handleError(e) {
+  console.log('uh oh');
+  $('#catkTarget').text('Failed to load cats, is the server working?');
+}
+
+function newCatSuccess(json) {
+  $('#newCatForm input').val('');
+  allCats.push(json);
+  render();
+}
+
+function newCatError() {
+  console.log('newbook error!');
+}
+
+function deleteCatSuccess(json) {
+  var cat = json;
+  console.log(json);
+  var catId = cat._id;
+  console.log('delete cat', catId);
+  for(var index = 0; index < allCats.length; index++) {
+    if(allCats[index]._id === catId) {
+      allCats.splice(index, 1);
+      break;  // we found our book - no reason to keep searching (this is why we didn't use forEach)
+    }
   }
-
-  function fetchAndReRenderCatWithId(catId) {
-  $.get('/api/cats/' + catId, function(data) {
-    // remove the current instance of the album from the page
-    $('div[data-cat-id=' + catId + ']').remove();
-    // re-render it with the new album data (including songs)
-    renderCat(data);
-  });
+  render();
 }
 
-$.ajax({
-   method: 'PUT',
-   url: '/api/cats/' + catId,
-   data: data,
-   success: handleCatUpdatedResponse
- });
-
-function handleCatUpdatedResponse(data) {
- console.log('response to update', data);
-
- var catId = data._id;
- // scratch this album from the page
- $('[data-cat-id=' + catId + ']').remove();
- // and then re-draw it with the updates ;-)
- renderCat(data);
-
- // BONUS: scroll the change into view ;-)
- $('[data-cat-id=' + catId + ']')[0].scrollIntoView();
-}
-
-  function handleDeleteCatClick(e) {
-  var catId = $(this).parents('.cat').data('cat-id');
-  console.log('someone wants to delete cat id=' + catId );
-  $.ajax({
-    url: '/api/cats/' + catId,
-    method: 'DELETE',
-    success: handleDeleteCatSuccess
-  });
-}
-// callback after DELETE /api/cats/:id
-function handleDeleteCatSuccess(data) {
-  var deletedCatId = data._id;
-  console.log('removing the following cat from the page:', deletedCatId);
-  $('div[data-cat-id=' + deletedCatId + ']').remove();
+function deleteCatError() {
+  console.log('delete cat error!');
 }
