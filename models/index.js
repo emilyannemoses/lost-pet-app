3085c76bd94b66cf95e4c0ae39db0fb5396b26f4
eamoses <e.a.moses@gmail.com>
2016-04-11 10:38:34 -0700

    trying to get it to show up on heroku

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 // Update your database connection to point to Heroku's database
 var mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/project-01");
 mongoose.connect( process.env.MONGOLAB_URI ||
                       process.env.MONGOHQ_URL ||
-                      "mongodb://protected-sea-74069.herokuapp.com/" );
+                      "mongodb://localhost/project-01" );
 
 var Cat = require('./cat');
 
